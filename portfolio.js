8db2cf9bdab2e850af1f60fc616bbac2e8ee9ef5
Kiran5b2 <147131427+Kiran5b2@users.noreply.github.com>
2024-09-18 19:37:44 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+
+ var typed = new Typed(".type1",{
+     strings : ["programmer","web developer","designer","frontend developer"],
+     typeSpeed : 150,
+     backSpeed : 150,
+     loop : true
+ })
